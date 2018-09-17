e9d2b764864fc986a9a0e83982eaa7e1d72b4db8
walk8243 <walk8243@gmail.com>
2018-09-17 18:07:57 +0900

    main経由で外部でpackage.jsonに適用を確認

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-const fs    = require('fs'),
-      init  = require('init-package-json');
+const init = require('./lib/init');
 
-const initFile = process.cwd() + '/npm-init.js',
-      initData = JSON.parse(fs.readFileSync(process.cwd() + '/package.json').toString());
-
-init(process.cwd(), initFile, initData, function (err, data) {
-  if (err) console.error(err);
-  // console.log('written successfully');
-});
+init()
+  .catch(error => console.error(error));
